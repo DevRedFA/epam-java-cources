c48fe49519ecc1402b65ab4fd4643f3788b293e3
DevRedFA <Sedykhivan@gmail.com>
2017-11-30 23:45:58 +0300

    Spring-reaper: Added annotation-config

--- src/main/java/com/epam/university/java/spring/quoters/TerminatorQuoter.java
+++ src/main/java/com/epam/university/java/spring/quoters/TerminatorQuoter.java
@@ -1,6 +1,12 @@
 package com.epam.university.java.spring.quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
+
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     private String message;
 
     public void setMessage(String message) {
@@ -9,8 +15,18 @@ public class TerminatorQuoter implements Quoter {
 
     @Override
     public void sayQuote() {
-        System.out.println("message = " + message);
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
     }
 
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
 }
